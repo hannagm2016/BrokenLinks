693731b56915d85e9d7d4b5e66b205d24d8c9100
hannagm2016 <74610938+hannagm2016@users.noreply.github.com>
2021-05-21 08:44:44 +0300

    added handeler

--- be/functions/ping.go
+++ be/functions/ping.go
@@ -6,7 +6,7 @@ import (
 	"net/http"
 	"strings"
 	"sync"
-"runtime"
+    "runtime"
 )
 
 
--- be/main.go
+++ be/main.go
@@ -1,44 +1,15 @@
 package main
 
 import (
-	"fmt"
+	h "main/be/handler"
 	"github.com/labstack/echo"
 	"github.com/labstack/echo/middleware"
-	"main/be/functions"
-	m "main/be/models"
-	"net/http"
-	"sync"
 )
 
-func Check(c echo.Context) error {
-	Result := new(m.Results)
-
-	wg := new(sync.WaitGroup)
-    mu:=new(sync.Mutex)
-	request := new(m.Url)
-	if err := c.Bind(&request); err != nil {
-		return err
-	}
-	Result.MainURL = request.Uri
-	res, err := http.Get(request.Uri)
-	defer res.Body.Close()
-	if err != nil {
-		fmt.Println(err)
-	}
-	ch := make(chan m.Url, 100)
-	go functions.Links(res, request.Level, wg, ch, Result)
-	for c := range ch {
-		go functions.Ping(wg, c, ch, mu, Result)
-	}
-	fmt.Println(Result.BrokenLinks, len(Result.BrokenLinks), "BrokenLinks")
-	fmt.Println(Result.AllLinks, len(Result.AllLinks), "AllLinks")
-
-	return c.JSON(http.StatusOK, Result.BrokenLinks)
-}
 func main() {
 	e := echo.New()
 	e.Use(middleware.CORS())
-	e.POST("/post", Check)
+	e.POST("/post", h.Check)
 	e.Static("/", "../fe/dist/")
 	e.Logger.Fatal(e.Start(":8000"))
 
